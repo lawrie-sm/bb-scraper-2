dc7feaf5231b8ab214d6ad91e6d141368943d0fd
lawrie-sm <alertrelic@gmail.com>
2018-03-19 13:45:06 +0000

    Started adding party selector

--- holyrood/searcher/static/searcher/scripts/Main.js
+++ holyrood/searcher/static/searcher/scripts/Main.js
@@ -10,34 +10,47 @@ $(function() {
 $(function() {
   $('#results-chart').on('submit', function(e) {
       e.preventDefault();
-      var data = $('#results-chart').serializeArray();
-      setupChart(RECIEVED_DATA, END_YEAR, data[0].value)
+      var formData = $('#results-chart').serializeArray();
+      console.log(formData);
+      setupChart(RECIEVED_DATA, END_YEAR, formData);
       return false;
   });
 });
 
-function setupChart(data, lastYearWithFullData, timeSpan) {
+function setupChart(data, lastYearWithFullData, formData) {
 
-  console.log('JSON DATA:')
-  console.log(data);
+  //console.log('JSON DATA:')
+  //console.log(data);
 
+  // Setup time span variables based on selection
   // 'last-year' is the default time-span
   var finalEndDate = moment(`01-01-${lastYearWithFullData}`);
   var timeStepMonths = 1;
   var updatingStartDate = moment(finalEndDate).subtract(1, 'years'); // moment(`01-01-1999`);
-  if (timeSpan != 'last-year') {
-    if (timeSpan === 'last-3-years') {
-      timeStepMonths = 3;
-      updatingStartDate = moment(finalEndDate).subtract(3, 'years');
-    } else if (timeSpan === 'last-10-years') {
-      timeStepMonths = 12;
-      updatingStartDate = moment(finalEndDate).subtract(10, 'years');
-    } else if (timeSpan === 'all-time') {
-      timeStepMonths = 12;
-      updatingStartDate = moment(`01-01-1999`);
+
+  // Setup default party data
+  var parties = ['snp', 'lab', 'con', 'ld', 'green', 'others'];
+
+  // Parse form data
+    if (formData) {
+    var timeSpan = formData[0].value;
+    if (timeSpan != 'last-year') {
+      if (timeSpan === 'last-3-years') {
+        timeStepMonths = 3;
+        updatingStartDate = moment(finalEndDate).subtract(3, 'years');
+      } else if (timeSpan === 'last-10-years') {
+        timeStepMonths = 12;
+        updatingStartDate = moment(finalEndDate).subtract(10, 'years');
+      } else if (timeSpan === 'all-time') {
+        timeStepMonths = 12;
+        updatingStartDate = moment(`01-01-1999`);
+      }
     }
+    parties = formData.map(function (d) {
+      if (d.name === 'partyCheckboxes') return d.value;
+    });
   }
-  
+
   var dateRanges = [];
   var isYear = (timeStepMonths === 12)
   while (finalEndDate > updatingStartDate ||
@@ -48,20 +61,29 @@ function setupChart(data, lastYearWithFullData, timeSpan) {
     endDate.add(timeStepMonths,'month')
     dateRanges.push({
       start: startDate,
-      end: endDate,
-      quantity: 0
+      end: endDate
     })
     updatingStartDate = endDate.get();
   }
   for (var i = 0; i < data.length; i++) {
     var itemMonth = moment(data[i].fields.date);
-    var range = dateRanges.find((dr) => itemMonth.isBetween(dr.start, dr.end))
-    if (range) range.quantity++;
+    var range = dateRanges.find(function(dr) {
+      return(itemMonth.isBetween(dr.start, dr.end));
+      });
+    if (range) {
+      if (!range.quantity) range.quantity = 0;
+      range.quantity++;
+    }
   }
 
-  var labels = dateRanges.map((dr) => isYear ? dr.start.format('YY') : dr.start.format('YY-MM'))
-  if (labels.length > 0) {  
-    var series = dateRanges.map((dr) => dr.quantity)
+  var labels = dateRanges.map(function(dr) {
+    return(isYear ? dr.start.format('YY') : dr.start.format('YY-MM'))
+  });
+
+  if (labels.length > 0) {
+    var series = dateRanges.map(function (dr) {
+      return(dr.quantity)
+    });
     var chartData = {
       labels,
       series: [ series ]
@@ -70,8 +92,8 @@ function setupChart(data, lastYearWithFullData, timeSpan) {
     new Chartist.Line('.ct-chart', chartData, chartArgs);
     $('.ct-chart').show();
     $('#no-data-warning').hide();
-    console.log('CHART DATA:')
-    console.log(chartData);
+    //console.log('CHART DATA:')
+    //console.log(chartData);
   } else {
     $('.ct-chart').hide();
     $('#no-data-warning').show();
