f2fec469cd264ad25ed985fb9a9eafc063b4eb2d
lawrie-sm <alertrelic@gmail.com>
2018-03-28 10:54:31 +0100

    Updated script

--- searcher/management/commands/getspdata.py
+++ searcher/management/commands/getspdata.py
@@ -9,9 +9,9 @@ from django.core.management import call_command
 class Command(BaseCommand):
     MIN_CHARS_FOR_ENTRY = 128
     help = 'Gets historical data from the SP APIs'
-    CONTRIB_YEARS = ['2012','2013', '2014', '2015', '2016', '2017']
+    CONTRIB_TEST_YEARS = ['2012','2013', '2014', '2015', '2016', '2017']
 
-    CONTRIB_FULL_YEARS = [
+    CONTRIB_YEARS = [
         '1999', '2000', '2001', '2002', '2003',
         '2004','2005', '2006', '2007', '2008',
         '2009', '2010', '2011', '2012','2013',
